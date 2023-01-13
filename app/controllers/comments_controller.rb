89b970c61854c4c583fe7120bf58d0b7b34e1f74
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2023-01-13 11:06:01 +0900

    Commentsコントローラのdestroy内部の変数修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,8 @@
 
 class CommentsController < ApplicationController
   def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
     redirect_to @commentable, notice: 'Comment was successfully destroyed.'
   end
 end
