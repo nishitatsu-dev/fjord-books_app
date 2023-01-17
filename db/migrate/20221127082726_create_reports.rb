775dfd8eb911e06c80af3967725472c2fdf3de88
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2023-01-17 19:07:14 +0900

    migrationファイルの修正

--- db/migrate/20221127082726_create_reports.rb
+++ db/migrate/20221127082726_create_reports.rb
@@ -1,7 +1,7 @@
 class CreateReports < ActiveRecord::Migration[6.1]
   def change
     create_table :reports do |t|
-      t.integer :user_id
+      t.integer :user_id, null: false
       t.string :title
       t.text :body
 
--- db/migrate/20221210003422_change_column_null_to_reports.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNullToReports < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :reports, :user_id, false
-  end
-end
