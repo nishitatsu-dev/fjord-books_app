79407e9a6754b418ce0a31c3d5fb16236888f7e4
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2023-01-18 15:58:07 +0900

    ReportとUserのモデルテストと、日報関連のシステムテストを追加
    テストの動作初期化
    
    Userクラス name_or_emailメソッドのテスト
    
    rubocop対応
    
    Userクラス follow, unfollowメソッドのテスト
    
    name or emailのテスト名にメソッド名を追記
    
    Userクラス following?, followed_by?メソッドのテスト
    
    Reportクラス edidable?, created_onメソッドのテスト
    
    bundle update webdriversを実行した
    
    システムテスト実行時のエラー対応（chrome側の問題。gemのアプデで対応）
    
    システムテストの書き方練習（books）
    
    Revert "システムテストの書き方練習（books）"
    
    This reverts commit 21d30eadcb960fcd694439efcfbcd71f2907f1b1.
    
    Reportのシステムテストを作成した
    
    Bookのシステムテストが走らないようにコメントアウトした
    
    erblintの導入
    
    erblint対応
    
    rubocop対応

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @me = User.create!(email: 'me@example.com', password: 'password')
+    @she = User.create!(email: 'she@example.com', password: 'password')
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '名前またはemailを返す__name_or_email' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal('foo@example.com', user.name_or_email)
+
+    user.name = 'Foo Bar'
+    assert_equal('Foo Bar', user.name_or_email)
+  end
+
+  test '他ユーザをフォローする__follow' do
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
+  end
+
+  test '他ユーザをフォロー解除する__unfollow' do
+    @me.follow(@she)
+    assert @me.following?(@she)
+    @me.unfollow(@she)
+    assert_not @me.following?(@she)
+  end
+
+  test 'あるユーザをフォローしている状態か確認__following?' do
+    assert @alice.following?(@bob)
+  end
+
+  test 'あるユーザにフォローされている状態か確認__followed_by?' do
+    assert @bob.followed_by?(@alice)
+  end
 end
